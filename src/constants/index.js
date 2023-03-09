c786453db0c947e6dd96657225a5df9a0181f44b
shikhar0512L <raginisingh92095@gmail.com>
2023-03-09 12:21:29 +0530

    Done!

---
+++ src/constants/index.js
@@ -0,0 +1,158 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    nodejs,
+    mongodb,
+    java,
+    threejs,
+    python,
+    android,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "android",
+      icon: android,
+    },
+    {
+      name: "java",
+      icon: java,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      icon: reactjs,
+      iconBg: "#383E56",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "React Native Developer",
+      icon: reactjs,
+      iconBg: "#E6DEDD",
+      points: [
+        "Developing and maintaining web applications using React Native and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      icon: web,
+      iconBg: "#383E56",
+      points: [
+        "Developing and maintaining web applications using React.js , Next.js and other related technologies.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Android Developer",
+      icon: android,
+      iconBg: "#E6DEDD",
+      points: [
+        "Developing and maintaining Android Applications using Android studio using Java and other related technologies.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Shikhar is best for the new programmers he uplode many tutorials of coding , project and more and he always respond to every comments and on discord . I would definietly recommend to learn from him",
+      name: "Hemant S. Rathod",
+    },
+    {
+      testimonial:
+        "Learning with Shikhar has been AWESOME, he makes learning React so easy and approachable, you'll finally feel like you know what's going on!",
+      name: "Adam Curtis",
+    }
+  ];
+  
+
+  
+  export { services, technologies, experiences, testimonials};
